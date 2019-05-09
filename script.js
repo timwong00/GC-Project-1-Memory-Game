b9799c0c09935943fb795ce6d1f6f336ea7f2d1e
jeff <jeffrileyjr80@live.com>
2019-05-09 16:51:09 -0400

    still working on fixing flip

--- script.js
+++ script.js
@@ -49,27 +49,37 @@ gameGrid.sort(() => 0.5 - Math.random());
 
 const grid = document.getElementById("game");  //accesses the section in HTML with ID game
 //adds the class grid to our sectiom element
-grid.classList.add("grid");
+// grid.classList.add("grid");
 gameGrid.forEach(item => {                       //for each image in our array
     const card = document.createElement("div");  //we create a div with a class card
     card.classList.add("card");
     card.dataset.name = item.name;
     const front = document.createElement("div"); //another div for the front of the card with class front
     front.classList.add("front");
+    front.style.backgroundImage = `url(./images/cardback.jpg)`;
     const back = document.createElement("div");  //and another div with the class back
     back.classList.add("back");
     back.style.backgroundImage = `url(${item.img})`;  //sets the image for the back of the card
-    // card.style.backgroundImage = `url(${item.img})`;
     grid.appendChild(card);
     card.appendChild(front);
     card.appendChild(back);
 });
 let userPicks = 0;  //declares a variable to try user choices, sets it at 0
-grid.addEventListener("click", function(event) {
+let card1 = null;
+let card2 = null;
+
+grid.addEventListener("click", function (event) {
+    console.log(event);
     let clicked = event.target;
     if (userPicks < 2) {  //makes sure user only gets 2 picks
         userPicks++;       //increases user picks
-        clicked.parentNode.classList.add("clickedOn");  //addes classlist clickedon to parent of div clicked on
-        console.log(userPicks);  //not needed, just tracking functionality
+        if (userPicks === 1) {
+            card1 = clicked.dataset.name;
+            console.log(card1);
+            clicked.classList.add("clickedOn");
+        } if (userPicks === 2) {
+            card2 = clicked.dataset.name;
+            clicked.classList.add("clickedOn");
+        }
     }
-  });
+});
