c737bbf8b5060dfa9c6a09063fe26e6e373a218e
tdub317 <wong.hsw@gmail.com>
2019-05-09 10:23:22 -0400

    changed 5x4 grid to 4x5 grid

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 function drawCardGrid() {
     const cardGrid = document.querySelector("section.card_grid");
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 5; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
         cardGrid.append(row);
-        for (let j = 0; j < 5; j++) {
+        for (let j = 0; j < 4; j++) {
             let card = document.createElement("div");
             card.classList.add("card");
             row.append(card);
@@ -15,3 +15,4 @@ function drawCardGrid() {
 }
 
 drawCardGrid();
+
