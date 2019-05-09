f56d09376cba0814e8e2fbe82892f1acdf0c1385
tdub317 <wong.hsw@gmail.com>
2019-05-09 14:51:51 -0400

    merged styleHTML CSS and fixed some HTML

--- script.js
+++ script.js
@@ -1,81 +1,75 @@
 "use strict";
-
-<<<<<<< HEAD
-var interval;
-
-function countdown() {
-    clearInterval(interval);
-    interval = setInterval(function () {
-        var timer = $('.js-timeout').html();
-        timer = timer.split(':');
-        var minutes = timer[0];
-        var seconds = timer[1];
-        seconds -= 1;
-        if (minutes < 0) return;
-        else if (seconds < 0 && minutes != 0) {
-            minutes -= 1;
-            seconds = 59;
-        }
-        else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
-
-        $('.js-timeout').html(minutes + ':' + seconds);
-
-        if (minutes == 0 && seconds == 0) clearInterval(interval);
-    }, 1000);
-}
-
-$('#js-startTimer').click(function () {
-    $('.js-timeout').text("2:00");
-    countdown();
-});
-
-$('#js-resetTimer').click(function () {
-    $('.js-timeout').text("2:00");
-    clearInterval(interval);
-});
-=======
-// function drawCardGrid() {
-//     const cardGrid = document.querySelector("section.card_grid");
-//     for (let i = 0; i < 5; i++) {
-//         let row = document.createElement("div");
-//         row.classList.add("row");
-//         cardGrid.append(row);
-//         for (let j = 0; j < 4; j++) {
-//             let card = document.createElement("div");
-//             card.classList.add("card");
-//             row.append(card);
-//         }
-//     }
-// }
-
-// drawCardGrid();
-
-window.onload = drawCardGrid();
-
-function drawCardGrid() {
-    const cardGrid = document.querySelector("section.card_grid");
-    if (window.innerWidth > 768) {
-        for (let i = 0; i < 4; i++) {
-            let row = document.createElement("div");
-            row.classList.add("row");
-            cardGrid.append(row);
-            for (let j = 0; j < 5; j++) {
-                let card = document.createElement("div");
-                card.classList.add("card");
-                row.append(card);
-            }
-        }
-    } else {
-        for (let i = 0; i < 5; i++) {
-            let row = document.createElement("div");
-            row.classList.add("row");
-            cardGrid.append(row);
-            for (let j = 0; j < 4; j++) {
-                let card = document.createElement("div");
-                card.classList.add("card");
-                row.append(card);
-            }
-        }
+//array containing all the card data and images
+const cardArray = [
+    {
+        name: "Stark",
+        img: "images/1.png"
+    },
+    {
+        name: "Lannister",
+        img: "images/2.png"
+    },
+    {
+        name: "Baratheon",
+        img: "images/3.png"
+    },
+    {
+        name: "Targaryen",
+        img: "images/4.png"
+    },
+    {
+        name: "Martell",
+        img: "images/5.png"
+    },
+    {
+        name: "Tyrell",
+        img: "images/6.png"
+    },
+    {
+        name: "Greyjoy",
+        img: "images/7.png"
+    },
+    {
+        name: "Tully",
+        img: "images/8.png"
+    },
+    {
+        name: "Arryn",
+        img: "images/9.png"
+    },
+    {
+        name: "Hodor",
+        img: "images/10.png"
     }
-}
->>>>>>> styleHTML
+];
+
+
+let gameGrid = cardArray.concat(cardArray);
+gameGrid.sort(() => 0.5 - Math.random());
+
+const grid = document.getElementById("game");  //accesses the section in HTML with ID game
+//adds the class grid to our sectiom element
+grid.classList.add("grid");
+gameGrid.forEach(item => {                       //for each image in our array
+    const card = document.createElement("div");  //we create a div with a class card
+    card.classList.add("card");
+    card.dataset.name = item.name;
+    const front = document.createElement("div"); //another div for the front of the card with class front
+    front.classList.add("front");
+    const back = document.createElement("div");  //and another div with the class back
+    back.classList.add("back");
+    back.style.backgroundImage = `url(${item.img})`;  //sets the image for the back of the card
+    // card.style.backgroundImage = `url(${item.img})`;
+    grid.appendChild(card);
+    card.appendChild(front);
+    card.appendChild(back);
+});
+let userPicks = 0;
+grid.addEventListener("click", function (event) {
+    let clicked = event.target;
+    if (userPicks < 2) {
+        userPicks++;
+        clicked.parentNode.classList.add("clickedOn");
+        console.log(userPicks);
+    }
+});
