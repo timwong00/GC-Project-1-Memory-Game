3a67fb0ecd60b2b701b6da4f22eee3b7aec6c4ed
tdub317 <wong.hsw@gmail.com>
2019-05-09 11:23:42 -0400

    grid will load 4x5 on mobile and 5x4 on desktop

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 "use strict";
 
+// function drawCardGrid() {
+//     const cardGrid = document.querySelector("section.card_grid");
+//     for (let i = 0; i < 5; i++) {
+//         let row = document.createElement("div");
+//         row.classList.add("row");
+//         cardGrid.append(row);
+//         for (let j = 0; j < 4; j++) {
+//             let card = document.createElement("div");
+//             card.classList.add("card");
+//             row.append(card);
+//         }
+//     }
+// }
+
+// drawCardGrid();
+
+window.onload = drawCardGrid();
+
 function drawCardGrid() {
     const cardGrid = document.querySelector("section.card_grid");
-    for (let i = 0; i < 5; i++) {
-        let row = document.createElement("div");
-        row.classList.add("row");
-        cardGrid.append(row);
-        for (let j = 0; j < 4; j++) {
-            let card = document.createElement("div");
-            card.classList.add("card");
-            row.append(card);
+    if (window.innerWidth > 768) {
+        for (let i = 0; i < 4; i++) {
+            let row = document.createElement("div");
+            row.classList.add("row");
+            cardGrid.append(row);
+            for (let j = 0; j < 5; j++) {
+                let card = document.createElement("div");
+                card.classList.add("card");
+                row.append(card);
+            }
+        }
+    } else {
+        for (let i = 0; i < 5; i++) {
+            let row = document.createElement("div");
+            row.classList.add("row");
+            cardGrid.append(row);
+            for (let j = 0; j < 4; j++) {
+                let card = document.createElement("div");
+                card.classList.add("card");
+                row.append(card);
+            }
         }
     }
 }
-
-drawCardGrid();
-
